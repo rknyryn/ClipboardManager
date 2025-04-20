3b5d4f3f644e8a72c17d25248fa9146b6da5e14a
rknyryn <rknyryn@gmail.com>
2025-04-20 15:30:12 +0300

    Initial Commit

---
+++ ClipboardManager/ClipboardManagerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ClipboardManagerApp.swift
+//  ClipboardManager
+//
+//  Created by Kaan Yarayan on 20.04.2025.
+//
+
+import SwiftUI
+
+@main
+struct ClipboardManagerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ClipboardManagerTests/ClipboardManagerTests.swift
@@ -0,0 +1,17 @@
+//
+//  ClipboardManagerTests.swift
+//  ClipboardManagerTests
+//
+//  Created by Kaan Yarayan on 20.04.2025.
+//
+
+import Testing
+@testable import ClipboardManager
+
+struct ClipboardManagerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
